8d19a1f3f2d38dc02f247c6cd75579cfcb87ec87
aakash <akash.gangule3@gmail.com>
2026-06-26 11:33:46 +0530

    practice on demoqa webpage

--- src/test/java/Locators_Practice/Demo4.java
+++ src/test/java/Locators_Practice/Demo4.java
@@ -1,16 +1,55 @@
 package Locators_Practice;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demo4 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
+		//Launch the browser
 		WebDriver driver=new ChromeDriver();
 		
-		driver.get("");
+		//enter the url
+		driver.get("https://demoqa.com/text-box");
 		
+		driver.manage().window().maximize();
 		
+		//wait for 2 sec
+		Thread.sleep(2000);
+		
+		//give the input in full name
+		driver.findElement(By.xpath("//input[@id='userName']")).sendKeys("Virat Kohli");
+		
+		//give the email
+		driver.findElement(By.xpath("//input[@id='userEmail']")).sendKeys("virat.kohli@rcb.com");
+		
+		//give the current address
+		driver.findElement(By.xpath("//textarea[@id='currentAddress']")).sendKeys("Alibag");
+
+		//enter the permanent address
+		driver.findElement(By.xpath("//textarea[@id='permanentAddress']")).sendKeys("Delhi");
+		
+		//Thread.sleep(1000);
+		
+		//click on the submit button
+		//driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
+		driver.findElement(By.xpath("//button[@id='submit']")).click();
+		
+		//wait for 2 sec
+		Thread.sleep(2000);
+		
+		//close the tab
+		driver.close();
 	}
 
 }
+
+
+
+
+
+
+
+
+
