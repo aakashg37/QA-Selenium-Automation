2cf9c7cd04c0ca19fc21d6b466805888b76b9ac3
aakash <akash.gangule3@gmail.com>
2026-06-26 11:54:37 +0530

    automated demoqa home page

--- src/test/java/Locators_Practice/Demo5.java
+++ src/test/java/Locators_Practice/Demo5.java
@@ -1,15 +1,62 @@
 package Locators_Practice;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demo5 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		WebDriver driver=new ChromeDriver();
 		
-		driver.get("");
+		//enter the url
+		driver.get("https://demoqa.com/");
 		
+		driver.manage().window().maximize();
+		
+		//wait for 1 sec
+		Thread.sleep(1000);
+				
+		//click on the elements tab
+		driver.findElement(By.xpath("//a[@href='/elements']")).click();
+		
+		Thread.sleep(2000);		
+		driver.navigate().back();
+		
+		//click on the forms tab
+		driver.findElement(By.xpath("//a[@href='/forms']")).click();
+				
+		Thread.sleep(2000);				
+		driver.navigate().back();
+		
+		//click on the alertswindows tab
+		driver.findElement(By.xpath("//a[@href='/alertsWindows']")).click();
+				
+		Thread.sleep(2000);				
+		driver.navigate().back();
+		
+		//click on the widgets tab
+		driver.findElement(By.xpath("//a[@href='/widgets']")).click();
+						
+		Thread.sleep(2000);						
+		driver.navigate().back();
+		
+		//click on the interaction tab
+		driver.findElement(By.xpath("//a[@href='/interaction']")).click();
+						
+		Thread.sleep(2000);
+		driver.navigate().back();
+		
+		driver.close();
 		
 	}
 }
+
+
+
+
+
+
+
+
+
