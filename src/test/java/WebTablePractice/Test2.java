4e2970a583bcf2b94f8db3f6e97db3d4afa89d2f
aakash <akash.gangule3@gmail.com>
2026-07-30 22:59:53 +0530

    WebTable Practice (Calendar irctc)

--- src/test/java/WebTablePractice/Test2.java
+++ src/test/java/WebTablePractice/Test2.java
@@ -1,7 +1,10 @@
 package WebTablePractice;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test2 {
@@ -26,5 +29,44 @@ public class Test2 {
 		//locate the date in the table and click on it
 		driver.findElement(By.xpath("//table[contains(@class,'ui-datepicker-calendar')]/tbody/tr[5]/td[6]")).click();
 		
+		Thread.sleep(2000);
+		
+		//Identify All Classes and click on it
+		driver.findElement(By.xpath("//div[contains(@class,'ng-tns-c76-10 ui-dropdown')]")).click();
+		
+		Thread.sleep(2000);
+		
+		//Select the class from the dropdown
+		List<WebElement> allClasses=driver.findElements(By.xpath("//div[@class='ui-dropdown-items-wrapper ng-tns-c76-10']//p-dropdownitem"));
+		
+		System.out.println(allClasses.size());
+		
+		Thread.sleep(2000);
+		
+		for(WebElement Class : allClasses)
+		{
+			String text=Class.getText();
+			System.out.println(text);
+			
+			if(text.equals("AC First Class (1A)")) {
+				
+				Class.click();
+				break;				
+			}			
+			
+		}
+		
+		
+		
 	}
 }
+
+
+
+
+
+
+
+
+
+
