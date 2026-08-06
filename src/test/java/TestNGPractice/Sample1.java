fa250520bfce12629ef2ab932097c3ca3e78f0f8
aakash <akash.gangule3@gmail.com>
2026-08-06 22:51:16 +0530

    TestNG practice

---
+++ src/test/java/TestNGPractice/Sample1.java
@@ -0,0 +1,45 @@
+package TestNGPractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Sample1 {
+
+	static WebDriver driver;
+	
+	@Test(priority=-1)
+	public void launchChromeBrowser() {
+		driver=new ChromeDriver();
+		Reporter.log("Chrome browser launched",true);
+	}
+	
+	@Test
+	public void enterUrl() {
+		driver.get("https://kite.zerodha.com/");
+		Reporter.log("URL entered into browser: Kite Zerodha",true);
+	}
+	
+	@Test(priority=1)
+	public void enterUname() {
+		driver.findElement(By.cssSelector("#userid")).sendKeys("Virat K");
+		Reporter.log("Username entered",true);
+	}
+	
+	@Test(priority=2)
+	public void enterPass() {
+		driver.findElement(By.cssSelector("#password")).sendKeys("Virat@123");
+		Reporter.log("Password entered",true);
+	}
+	
+	
+}
+
+
+
+
+
+
+
---
+++ src/test/java/TestNGPractice/Sample2.java
@@ -0,0 +1,49 @@
+package TestNGPractice;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.google.common.io.Files;
+
+public class Sample2 {
+
+	static WebDriver driver;
+	
+	@Test(priority=-1)
+	public void launchChromeBrowser() {
+		driver=new ChromeDriver();
+		Reporter.log("Chrome browser launched",true);
+	}
+	
+	@Test
+	public void loginTest() {
+		driver.get("https://kite.zerodha.com/");
+		driver.findElement(By.cssSelector("#userid")).sendKeys("Virat K");
+		driver.findElement(By.cssSelector("#password")).sendKeys("Virat@123");
+		Reporter.log("Login Details entered",true);
+	}
+	
+	
+	@Test
+	public void takeLoginScreenshotTest() throws IOException {
+		
+		TakesScreenshot ts=(TakesScreenshot)driver;
+		File source=ts.getScreenshotAs(OutputType.FILE);
+		
+		File destination=new File("D:\\21stMarchFSST\\Screenshot\\LoginZerodha.png");
+		
+		Files.copy(source, destination);
+		
+		Reporter.log("Login Screen Screenshot taken successfully",true);
+	}
+	
+	
+}
